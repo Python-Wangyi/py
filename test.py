cdb2325c0e6cb9ec4512a94a0ee24146d9da6562
steven <496909030@qq.com>
2024-07-05 17:56:02 +0800

    test

--- test.py
+++ test.py
@@ -3,11 +3,20 @@ from time import time
 
 
 class TimeCounter(object):
-    def __init__(self, func):
-        self.func = func
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
-    def __call__(self, *args, **kwargs):
-        t = time()
-        self.func()
-        return print(f"time: {time() - t}")
+    def __call__(self, func):
+        def wrapper(*args, **kwargs):
+            t = time()
+            func()
+            return print(f"time: {time() - t}")
+        return wrapper
 
+@TimeCounter('a', 'b')
+def function():
+    print("hello world")
+
+
+function()
