fd2c9facb08345e47e4cbbbb6326cbf6728c3a0c
steven <496909030@qq.com>
2024-07-05 17:07:28 +0800

    test

--- test.py
+++ test.py
@@ -3,16 +3,16 @@ from time import time
 
 
 def time_counter(f):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         t1 = time()
-        f()
+        f(*args, **kwargs)
         t2 = time() - t1
         return print(f"运行时间：{t2}")
     return wrapper
 
 
-# @time_counter
+@time_counter
 def mainloop(n: str):
     print(n)
 
-mainloop()
+mainloop('aaaaaaaaaaaaaaaaaaaaaaaaa')
