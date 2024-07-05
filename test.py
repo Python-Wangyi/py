3789ec71e7a581e1709bbafe4360a1ea3d2e1ddf
steven <496909030@qq.com>
2024-07-05 17:18:47 +0800

    test

--- test.py
+++ test.py
@@ -2,17 +2,12 @@
 from time import time
 
 
-def time_counter(f):
-    def wrapper(*args, **kwargs):
-        t1 = time()
-        f(*args, **kwargs)
-        t2 = time() - t1
-        return print(f"运行时间：{t2}")
-    return wrapper
+class TimeCounter(object):
+    def __init__(self, func):
+        self.func = func
 
+    def __call__(self, *args, **kwargs):
+        t = time()
+        self.func()
+        return print(f"time: {time() - t}")
 
-@time_counter
-def mainloop(n: str):
-    print(n)
-
-mainloop('aaaaaaaaaaaaaaaaaaaaaaaaa')
